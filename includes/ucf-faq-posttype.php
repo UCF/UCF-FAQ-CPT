a82032c5e2d6de850675b3dd2a98e3ca461b053d
RJ Bruneel <rj.bruneel@ucf.edu>
2018-08-06 14:41:26 -0400

    Updated $text_domain to use a public static variable

--- includes/ucf-faq-posttype.php
+++ includes/ucf-faq-posttype.php
@@ -6,16 +6,17 @@
  **/
 if ( ! class_exists( 'UCF_FAQ_PostType' ) ) {
 	class UCF_FAQ_PostType {
+		public static $text_domain = 'ucf_faq';
+
 		/**
 		 * Registers the FAQ custom post type.
 		 * @author Jim Barnes
 		 * @since 1.0.0
 		 **/
 		public static function register() {
-			$text_domain = 'ucf_faq';
-			$singular = apply_filters( $text_domain . '_singular_label', 'FAQ' );
-			$plural = apply_filters( $text_domain . '_plural_label', 'FAQs' );
-			register_post_type( 'faq', self::args( $singular, $plural, $text_domain ) );
+			$singular = apply_filters( self::$text_domain . '_singular_label', 'FAQ' );
+			$plural = apply_filters( self::$text_domain . '_plural_label', 'FAQs' );
+			register_post_type( 'faq', self::args( $singular, $plural ) );
 		}
 
 		/**
@@ -24,32 +25,31 @@ if ( ! class_exists( 'UCF_FAQ_PostType' ) ) {
 		 * @since 1.0.0
 		 * @param $singular string | The singular form for the CPT labels.
 		 * @param $plural string | The plural form for the CPT labels.
-		 * @param $text_domain string | Text domain for CPT.
 		 * @return Array
 		 **/
-		public static function labels( $singular, $plural, $text_domain ) {
+		public static function labels( $singular, $plural ) {
 			return array(
-				'name'                  => _x( $plural, 'Post Type General Name', $text_domain ),
-				'singular_name'         => _x( $singular, 'Post Type Singular Name', $text_domain ),
-				'menu_name'             => __( $plural, $text_domain ),
-				'name_admin_bar'        => __( $singular, $text_domain ),
-				'archives'              => __( $plural . ' Archives', $text_domain ),
-				'parent_item_colon'     => __( 'Parent ' . $singular . ':', $text_domain ),
-				'all_items'             => __( 'All ' . $plural, $text_domain ),
-				'add_new_item'          => __( 'Add New ' . $singular, $text_domain ),
-				'add_new'               => __( 'Add New', $text_domain ),
-				'new_item'              => __( 'New ' . $singular, $text_domain ),
-				'edit_item'             => __( 'Edit ' . $singular, $text_domain ),
-				'update_item'           => __( 'Update ' . $singular, $text_domain ),
-				'view_item'             => __( 'View ' . $singular, $text_domain ),
-				'search_items'          => __( 'Search ' . $plural, $text_domain ),
-				'not_found'             => __( 'Not found', $text_domain ),
-				'not_found_in_trash'    => __( 'Not found in Trash', $text_domain ),
-				'insert_into_item'      => __( 'Insert into ' . $singular, $text_domain ),
-				'uploaded_to_this_item' => __( 'Uploaded to this ' . $singular, $text_domain ),
-				'items_list'            => __( $plural . ' list', $text_domain ),
-				'items_list_navigation' => __( $plural . ' list navigation', $text_domain ),
-				'filter_items_list'     => __( 'Filter ' . $plural . ' list', $text_domain ),
+				'name'                  => _x( $plural, 'Post Type General Name', self::$text_domain ),
+				'singular_name'         => _x( $singular, 'Post Type Singular Name', self::$text_domain ),
+				'menu_name'             => __( $plural, self::$text_domain ),
+				'name_admin_bar'        => __( $singular, self::$text_domain ),
+				'archives'              => __( $plural . ' Archives', self::$text_domain ),
+				'parent_item_colon'     => __( 'Parent ' . $singular . ':', self::$text_domain ),
+				'all_items'             => __( 'All ' . $plural, self::$text_domain ),
+				'add_new_item'          => __( 'Add New ' . $singular, self::$text_domain ),
+				'add_new'               => __( 'Add New', self::$text_domain ),
+				'new_item'              => __( 'New ' . $singular, self::$text_domain ),
+				'edit_item'             => __( 'Edit ' . $singular, self::$text_domain ),
+				'update_item'           => __( 'Update ' . $singular, self::$text_domain ),
+				'view_item'             => __( 'View ' . $singular, self::$text_domain ),
+				'search_items'          => __( 'Search ' . $plural, self::$text_domain ),
+				'not_found'             => __( 'Not found', self::$text_domain ),
+				'not_found_in_trash'    => __( 'Not found in Trash', self::$text_domain ),
+				'insert_into_item'      => __( 'Insert into ' . $singular, self::$text_domain ),
+				'uploaded_to_this_item' => __( 'Uploaded to this ' . $singular, self::$text_domain ),
+				'items_list'            => __( $plural . ' list', self::$text_domain ),
+				'items_list_navigation' => __( $plural . ' list navigation', self::$text_domain ),
+				'filter_items_list'     => __( 'Filter ' . $plural . ' list', self::$text_domain ),
 			);
 		}
 
@@ -61,11 +61,11 @@ if ( ! class_exists( 'UCF_FAQ_PostType' ) ) {
 		 * @param $plural string | The plural form for the CPT labels.
 		 * @return Array
 		 **/
-		public static function args( $singular, $plural, $text_domain ) {
+		public static function args( $singular, $plural ) {
 			$args = array(
-				'label'                 => __( $plural, $text_domain ),
-				'description'           => __( $plural, $text_domain ),
-				'labels'                => self::labels( $singular, $plural, $text_domain ),
+				'label'                 => __( $plural, self::$text_domain ),
+				'description'           => __( $plural, self::$text_domain ),
+				'labels'                => self::labels( $singular, $plural, self::$text_domain ),
 				'supports'              => array( 'title', 'editor', 'revisions', 'custom-fields', ),
 				'taxonomies'            => self::taxonomies(),
 				'hierarchical'          => false,
@@ -83,7 +83,7 @@ if ( ! class_exists( 'UCF_FAQ_PostType' ) ) {
 				'capability_type'       => 'post',
 			);
 
-			$args = apply_filters( $text_domain . '_post_type_args', $args );
+			$args = apply_filters( self::$text_domain . '_post_type_args', $args );
 
 			return $args;
 		}
@@ -100,7 +100,7 @@ if ( ! class_exists( 'UCF_FAQ_PostType' ) ) {
 				'category'
 			);
 
-			$taxonomies = apply_filters( 'ucf_faq_taxonomies', $taxonomies );
+			$taxonomies = apply_filters( self::$text_domain . '_taxonomies', $taxonomies );
 
 			foreach( $taxonomies as $taxonomy ) {
 				if ( ! taxonomy_exists( $taxonomy ) ) {
