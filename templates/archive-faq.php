093220206a167b31894b42bfb788b837f35640d7
RJ Bruneel <rj.bruneel@ucf.edu>
2018-08-21 15:30:21 -0400

    Added some flare

--- templates/archive-faq.php
+++ templates/archive-faq.php
@@ -5,8 +5,8 @@ $title_classes = " mb-4";
 ?>
 
 <article>
-	<div class="ucf-faq-list<?php UCF_FAQ_Config::add_athena_attr($container_classes); ?>">
-		<h1 class="ucf-faq-title<?php UCF_FAQ_Config::add_athena_attr($title_classes); ?>">Frequently Asked Questions</h1>
+	<div class="ucf-faq-list<?php UCF_FAQ_Config::add_athena_attr( $container_classes ); ?>">
+		<h1 class="ucf-faq-title<?php UCF_FAQ_Config::add_athena_attr( $title_classes ); ?>">Frequently Asked Questions</h1>
 		<?php
 		if ( have_posts() ) {
 
@@ -24,7 +24,7 @@ $title_classes = " mb-4";
 			foreach( $items as $key => $item ) :
 				$topic_title_classes = " mt-4 mb-3";
 			?>
-				<h2 class="ucf-faq-topic<?php UCF_FAQ_Config::add_athena_attr($topic_title_classes); ?>"><?php echo $key; ?></h2>
+				<h2 class="ucf-faq-topic<?php UCF_FAQ_Config::add_athena_attr( $topic_title_classes ); ?>"><?php echo $key; ?></h2>
 			<?php
 				foreach( $item as $post ) :
 					$question_classes = " mt-3 h5";
@@ -33,11 +33,11 @@ $title_classes = " mb-4";
 					$answer_attrs = ' id="post' . $post->ID . '"';
 			?>
 					<a href="<?php echo get_permalink( $post->ID ); ?>">
-						<h3 class="ucf-faq-question<?php UCF_FAQ_Config::add_athena_attr($question_classes); ?>"<?php UCF_FAQ_Config::add_athena_attr($question_attrs); ?>>
+						<h3 class="ucf-faq-question<?php UCF_FAQ_Config::add_athena_attr( $question_classes ); ?>"<?php UCF_FAQ_Config::add_athena_attr( $question_attrs ); ?>>
 							<?php echo $post->post_title; ?>
 						</h3>
 					</a>
-					<div class="ucf-faq-topic-answer<?php UCF_FAQ_Config::add_athena_attr($answer_classes); ?>"<?php UCF_FAQ_Config::add_athena_attr($answer_attrs); ?>>
+					<div class="ucf-faq-topic-answer<?php UCF_FAQ_Config::add_athena_attr( $answer_classes ); ?>"<?php UCF_FAQ_Config::add_athena_attr( $answer_attrs ); ?>>
 						<?php echo $post->post_content; ?>
 					</div>
 			<?php
