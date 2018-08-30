6caf71bf6648f822963b42f2a81c1cfab3454d4a
RJ Bruneel <rj.bruneel@ucf.edu>
2018-08-30 16:10:56 -0400

    Added related FAQs logic

--- templates/taxonomy-topic.php
+++ templates/taxonomy-topic.php
@@ -1,9 +1,35 @@
 <?php
 get_header();
+
+$tags = array();
+$faqs = array();
 $container_classes = " container mb-5";
 $show = ( get_field( 'faq-topic-show-answers', get_queried_object() ) ) ?  " show" : "";
 $topic_description = term_description();
+
+
+function display_faq( $post, $show, $question_class ) {
+	ob_start();
+
+	$question_classes = " mt-3 " . $question_class;
+	$question_attrs   = ' data-toggle="collapse" href="#post' . $post->ID . '"';
+	$answer_classes   = " mt-2 mb-4 collapse" . $show;
+	$answer_attrs     = ' id="post' . $post->ID . '"';
 ?>
+	<a href="<?php echo get_permalink( $post->ID ); ?>">
+		<h2 class="ucf-faq-question<?php UCF_FAQ_Config::add_athena_attr( $question_classes ); ?>"<?php UCF_FAQ_Config::add_athena_attr( $question_attrs ); ?>>
+			<?php echo $post->post_title; ?>
+		</h2>
+	</a>
+
+	<div class="ucf-faq-topic-answer<?php UCF_FAQ_Config::add_athena_attr( $answer_classes ); ?>"<?php UCF_FAQ_Config::add_athena_attr( $answer_attrs ); ?>>
+		<?php echo $post->post_content; ?>
+	</div>
+<?php
+	return ob_get_clean();
+}
+?>
+
 
 <article>
 	<div class="ucf-faq-topic-list<?php UCF_FAQ_Config::add_athena_attr( $container_classes ); ?>">
@@ -23,33 +49,55 @@ $topic_description = term_description();
 			<div class="<?php UCF_FAQ_Config::add_athena_attr( 'row' ); ?>">
 				<div class="<?php UCF_FAQ_Config::add_athena_attr( 'col-md-8' ); ?>">
 			<?php
-			while ( have_posts() ) {
-				the_post();
-				$question_classes = " mt-3 h4";
-				$question_attrs = ' data-toggle="collapse" href="#post' . $post->ID . '"';
-				$answer_classes = " mt-2 mb-4 collapse" . $show;
-				$answer_attrs = ' id="post' . $post->ID . '"';
+				while ( have_posts() ) {
+					the_post();
+
+					// Save FAQs and tags for related FAQs section
+					array_push( $faqs, $post );
+
+					foreach( wp_get_post_tags( $post->ID ) as $tag ) {
+						if( !in_array( $tag, $tags ) ) {
+							array_push( $tags, $tag->slug );
+						}
+					}
+
+					echo display_faq( $post, $show, 'h4' );
+				}
+
+				// Get posts with same tags to display in related FAQs section
+				$args = array(
+					'post_type'      => 'faq',
+					'posts_per_page' => -1,
+					'tax_query'      => array(
+						array(
+							'taxonomy' => 'post_tag',
+							'field'    => 'slug',
+							'terms'    => $tags,
+						),
+					),
+				);
+
+				$posts = get_posts( $args );
+				$related_faqs = null;
+
+				foreach( $posts as $post ) {
+					if( !in_array( $post, $faqs ) ) { // Don't display FAQs already on the page
+						$related_faqs =  display_faq( $post, $show, 'h5' );
+					}
+				}
+
+				if( $related_faqs ) {
+				?>
+					<h2 class="<?php UCF_FAQ_Config::add_athena_attr( 'h3 mt-5 mb-4' ); ?>">Related FAQs</h3>
+				<?php
+					echo $related_faqs;
+				}
+
 			?>
-				<a href="<?php echo get_permalink( $post->ID ); ?>">
-					<h2 class="ucf-faq-question<?php UCF_FAQ_Config::add_athena_attr( $question_classes ); ?>"<?php UCF_FAQ_Config::add_athena_attr( $question_attrs ); ?>>
-						<?php the_title(); ?>
-					</h2>
-				</a>
-				<div class="ucf-faq-topic-answer<?php UCF_FAQ_Config::add_athena_attr( $answer_classes ); ?>"<?php UCF_FAQ_Config::add_athena_attr( $answer_attrs ); ?>>
-					<?php the_content(); ?>
 				</div>
-			<?php
-			}
-			?>
-				</div>
+
 				<div class="<?php UCF_FAQ_Config::add_athena_attr( 'col-md-4 mt-3' ); ?>">
 					<h2 class="<?php UCF_FAQ_Config::add_athena_attr( 'h5' ); ?>">TODO: Spotlight</h3>
-				</div>
-			</div>
-			<div class="<?php UCF_FAQ_Config::add_athena_attr( 'row mt-4' ); ?>">
-				<div class="<?php UCF_FAQ_Config::add_athena_attr( 'col-md-4' ); ?>">
-					<h2 class="<?php UCF_FAQ_Config::add_athena_attr( 'h5' ); ?>">TODO: Related FAQs</h3>
-					<p>List of FAQs with the same topic.</p>
 				</div>
 			</div>
 			<?php
