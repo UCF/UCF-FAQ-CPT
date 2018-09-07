6e182259290cc3195a8decbbe30a587d4b612af3
RJ Bruneel <rj.bruneel@ucf.edu>
2018-09-07 14:33:41 -0400

    Added logic to hide the CTA if the text and link are missing

--- templates/single-faq.php
+++ templates/single-faq.php
@@ -6,13 +6,20 @@ $title_classes = " mb-4";
 $answer_classes = " col-lg-8 mt-2 mb-4";
 $tags = array();
 $topic = wp_get_post_terms( $post->ID, 'topic' );
+
 if( $topic && is_array( $topic ) ) {
 	$topic = $topic[0];
 }
 
+$cta_text = get_field( 'faq-topic-footer-cta-text', $topic );
+$cta_url = site_url() . get_field( 'faq-topic-footer-cta-url', $topic );
+
 foreach( wp_get_post_tags( $post->ID ) as $tag ) {
 	array_push( $tags, $tag->slug );
 }
+
+$related_posts = UCF_FAQ_Common::get_related_faqs( $tags, "" );
+$related_faq_html = null;
 ?>
 
 <article>
@@ -21,27 +28,28 @@ foreach( wp_get_post_tags( $post->ID ) as $tag ) {
 			<?php
 			echo apply_filters( 'the_content', $post->post_content );
 
-			$related_posts = UCF_FAQ_Common::get_related_faqs( $tags, "" );
-			$related_faq_html = null;
-
 			foreach( $related_posts as $post ) {
 				$related_faq_html .=  UCF_FAQ_Common::display_faq( $post, $show, 'h5' );
 			}
 
 			if( $related_faq_html ) :
 			?>
-					<h2 class="<?php UCF_FAQ_Config::add_athena_attr( 'h4 mt-5 mb-4' ); ?>">
-					<?php echo get_field( 'related-faq-title', $topic ); ?>
-					</h2>
+				<h2 class="<?php UCF_FAQ_Config::add_athena_attr( 'h4 mt-5 mb-4' ); ?>">
+				<?php echo get_field( 'related-faq-title', $topic ); ?>
+				</h2>
 			<?php
-					echo $related_faq_html;
-				endif;
+				echo $related_faq_html;
+			endif;
 
+			if( $cta_text && $cta_url ) :
 			?>
-			<a href="<?php echo site_url() . get_field( 'faq-topic-footer-cta-url', $topic ) ?>"
-				class="<?php UCF_FAQ_Config::add_athena_attr( 'btn btn-primary mt-4' ); ?>">
-				<?php echo get_field( 'faq-topic-footer-cta-text', $topic ) ?>
-			</a>
+				<a href="<?php echo $cta_url; ?>"
+					class="<?php UCF_FAQ_Config::add_athena_attr( 'btn btn-primary mt-4' ); ?>">
+					<?php echo $cta_text; ?>
+				</a>
+			<?php
+			endif;
+			?>
 		</div>
 	</div>
 </article>
