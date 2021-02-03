f39cb0529939f8347ecd282f725986eb6cb63bd2
Jo Dickson <jogreybill@gmail.com>
2021-02-03 15:01:34 -0500

    Added display method for faq searches for consistency with other shortcodes

--- common/ucf-faq-search-common.php
+++ common/ucf-faq-search-common.php
@@ -7,35 +7,24 @@
  **/
 if ( ! class_exists( 'UCF_FAQ_Search_Common' ) ) {
 	class UCF_FAQ_Search_Common {
-		public static function display_faq_search( $items, $layout, $args ) {
+		/**
+		 * Displays a FAQ search typeahead.
+		 * @since 1.3.1
+		 * @author Jo Dickson
+		 * @return string
+		 */
+		public static function display_faq_search( $args ) {
+			$title         = $args['title'];
+			$title_classes = $args['title_classes'];
+
+			$random_hash = substr( md5( rand( 0, 500 ) ), 0, 16 );
+			$id          = "faq-typeahead-$random_hash";
+
 			ob_start();
-
-			// // Display before
-			// $layout_before = ucf_faq_list_display_classic_before( '', $items, $args );
-			// if ( has_filter( 'ucf_faq_list_display_' . $layout . '_before' ) ) {
-			// 	$layout_before = apply_filters( 'ucf_faq_list_display_' . $layout . '_before', $layout_before, $items, $args );
-			// }
-			// echo $layout_before;
-
-			// // Display title
-			// $layout_title = ucf_faq_list_display_classic_title( '', $items, $args );
-			// if ( has_filter( 'ucf_faq_list_display_' . $layout . '_title' ) ) {
-			// 	$layout_title = apply_filters( 'ucf_faq_list_display_' . $layout . '_title', $layout_title, $items, $args );
-			// }
-			// echo $layout_title;
-
-			// $layout_content = ucf_faq_list_display_classic( '', $items, $args );
-			// if ( has_filter( 'ucf_faq_list_display_' . $layout ) ) {
-			// 	$layout_content = apply_filters( 'ucf_faq_list_display_' . $layout, $layout_content, $items, $args );
-			// }
-			// echo $layout_content;
-
-			// // Display after
-			// $layout_after = ucf_faq_list_display_classic_after( '', $items, $args );
-			// if ( has_filter( 'ucf_faq_list_display_' . $layout . '_after' ) ) {
-			// 	$layout_after = apply_filters( 'ucf_faq_list_display_' . $layout . '_after', $layout_after, $items, $args );
-			// }
-			// echo $layout_after;
+		?>
+			<label class="<?php echo $title_classes; ?>" for="<?php echo $id; ?>"><?php echo $title; ?></label>
+			<input id="<?php echo $id; ?>" class="faq-typeahead form-control" type="text" placeholder="<?php echo $args['placeholder']; ?>">
+		<?php
 			return ob_get_clean();
 		}
 	}
